91af58192c4383202471193fe20fb90ea3afa385
bbachdev <bbach7139@gmail.com>
2023-04-08 20:24:59 -0500

    1st Revisions of initial Sign Up step

--- src/components/SignUpSteps/UsernameStep.tsx
+++ src/components/SignUpSteps/UsernameStep.tsx
@@ -51,6 +51,7 @@ export default function UsernameStep() {
 
   async function submit() {
     //Call checkUsername to make sure the username is available (avoids abusing disabled state of button)
+    //TODO: Check if we need to call this; blur event might already be doing this (if it runs before submit)
     await checkUsername()
     if(usernameState === UsernameState.Available) {
       //Save information to database (TODO) and redirect to next step
@@ -59,42 +60,53 @@ export default function UsernameStep() {
   }
 
   return (
-    <div className={'shadow-md flex flex-col p-2 bg-white'}>
-      <span className={'font-bold mx-auto'}>
+    <div className={'shadow-md flex flex-col p-2 px-8 pb-4 bg-white'}>
+      <span className={'text-xl font-thin mx-auto mb-8'}>
         {`Welcome! Let's Get Started.`}
       </span>
-      <div className={'flex flex-col space-y-2'}>
-        <div className={'flex flex-row relative items-center'}>
-          <input placeholder='Username' className={'p-2 pr-12 grow rounded-md border-2 border-gray-300'} onBlur={checkUsername} onChange={(e) => setUsername(e.target.value)}/>
-          {/* Username check icons */}
-          {usernameState === UsernameState.Available && <CheckIcon className={'absolute right-2'} /> }
-          {usernameState === UsernameState.Taken && <CloseIcon className={'absolute right-2'} /> }
-          {usernameState === UsernameState.Checking && <LoopIcon className={'absolute right-2 animate-spin rotate-180 rotate'} /> }
+      <div className={'flex flex-col space-y-8'}>
+        <div className={'flex flex-col'}>
+          <span>Username</span>
+          <div className={'flex flex-row relative items-center'}>
+            <input className={'p-2 pr-12 grow rounded-md border-2 border-gray-300 bg-gray-100'} onBlur={checkUsername} onChange={(e) => setUsername(e.target.value)}/>
+            {/* Username check icons */}
+            {usernameState === UsernameState.Available && <CheckIcon className={'absolute right-2'} /> }
+            {usernameState === UsernameState.Taken && <CloseIcon className={'absolute right-2'} /> }
+            {usernameState === UsernameState.Checking && <LoopIcon className={'absolute right-2 animate-spin rotate-180 rotate'} /> }
+          </div>
         </div>
-        <input placeholder='Email' className={'p-2 rounded-md border-2 border-gray-300'}/>
-        {/* <input type='date' placeholder='Date of Birth' className={'p-2 rounded-md border-2 border-gray-300'}/> */}
-        <div className={'flex flex-row space-x-2'}>
-          <select className={'p-2 rounded-md border-2 border-gray-300 bg-white'} onChange={(e) => setBirthMonth(e.target.value)}>
-            {months.map((month) => {
-              return <option key={month} value={month}>{month}</option>
-            })}
-          </select>
-          {/* Upon month being selected, modify day list */}
-          <select className={'p-2 rounded-md border-2 border-gray-300 bg-white'} onChange={(e) => setBirthDay(parseInt(e.target.value))}>
-            {dayList.map((day) => {
-              return <option key={day} value={day}>{day}</option>
-            })}
-          </select>
-          <select className={'p-2 rounded-md border-2 border-gray-300 bg-white'} onChange={(e) => setBirthYear(parseInt(e.target.value))}>
-            {Array.from({ length: 120 }, (_, index) => new Date().getFullYear() - index).map((year, idx) => {
-              return <option key={year} value={year}>{year}</option>
-            })}
-          </select>
+        
+        <div className={'flex flex-col'}>
+          <span>Email</span>
+          <input className={'p-2 rounded-md border-2 border-gray-300 bg-gray-100'}/>
+        </div>
+        
+        <div className={'flex flex-col'}>
+          <span>Date of Birth</span>
+          <span className={'text-sm text-gray-400 leading-none mb-2'}>{`Your Date of Birth will not be shown publicly without permission. For business or "non-human" accounts, enter the Date of Birth of the primary account operator.`}</span>
+          <div className={'flex flex-row space-x-2'}>
+            <select className={'p-2 rounded-md border-2 border-gray-300 bg-gray-100'} onChange={(e) => setBirthMonth(e.target.value)}>
+              {months.map((month) => {
+                return <option key={month} value={month}>{month}</option>
+              })}
+            </select>
+            {/* Upon month being selected, modify day list */}
+            <select className={'p-2 rounded-md border-2 border-gray-300 bg-gray-100'} onChange={(e) => setBirthDay(parseInt(e.target.value))}>
+              {dayList.map((day) => {
+                return <option key={day} value={day}>{day}</option>
+              })}
+            </select>
+            <select className={'p-2 rounded-md border-2 border-gray-300 bg-gray-100'} onChange={(e) => setBirthYear(parseInt(e.target.value))}>
+              {Array.from({ length: 120 }, (_, index) => new Date().getFullYear() - index).map((year, idx) => {
+                return <option key={year} value={year}>{year}</option>
+              })}
+            </select>
+          </div>
         </div>
       </div>
 
       <div className={'flex flex-row mt-4'}>
-        <button className={' ml-auto bg-brand-green-400 hover:bg-brand-green-500 disabled:bg-gray-400 text-white p-2 rounded-md'} disabled={usernameState !== UsernameState.Available} onClick={submit}>
+        <button className={' ml-auto bg-brand-green-400 hover:bg-brand-green-500 disabled:bg-gray-400 text-white p-2 rounded-md px-4'} disabled={usernameState !== UsernameState.Available} onClick={submit}>
           {`Next`}
         </button>
       </div>
